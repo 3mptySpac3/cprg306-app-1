5d7feea34296a3d2c4075fb536d7060c25837425
3mptySpac3 <thefantasticalproject@gmail.com>
2023-09-22 16:32:00 -0600

    more changes

--- app/page.js
+++ app/page.js
@@ -5,18 +5,19 @@ import Link from 'next/link';
 
 export default function Page() {
   return (
-    
       <main>
-        < Navbar />
-      <h1 className = 'text-4xl m-2'>CPRG 306: Web Development 2 - Assignments</h1>
+      <Navbar />
+      <div className = "mt-24">
+        <h1 className = 'text-4xl m-2 ml-36'>CPRG 306: Web Development 2 - Assignments</h1>
 
-      <div>
-        <p>Name: Jean-Pierre Nde-Forgwang</p>
-        <p>Course Section: CPRG 306 A</p>
-        <p>
-          <Link href="https://github.com/">GitHub</Link> 
-        </p>
-      </div>
+        <div>
+          <p>Name: Jean-Pierre Nde-Forgwang</p>
+          <p>Course Section: CPRG 306 A</p>
+          <p>
+            <Link href="https://github.com/">GitHub</Link> 
+          </p>
+        </div>
+        </div>
 
       </main>
     
