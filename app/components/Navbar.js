6e1b16e9035cbeedb39d5ebdcd3725693b04d7c4
3mptySpac3 <thefantasticalproject@gmail.com>
2023-09-21 20:23:57 -0600

    fixing a few things
    moved some things around, deleted studentinfo, opting to use our lord and savior chat

---
+++ app/components/Navbar.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+function Navbar(){
+  return(
+    <nav>
+      <link href='/'><a>Home</a></link>
+      <link href='/week'><a>About</a></link>
+    </nav>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { NavigationMenu } from './NavigationMenu';
+import Link from 'next/link';
+
 
 export default function Page() {
   return (
     <main>
-    <NavigationMenu />
-    <h1 className = 'text-4xl m-2'>Page</h1>
-    <p>This is the page page</p>
-    <p>This is another</p>
+    <h1 className = 'text-4xl m-2'>CPRG 306: Web Development 2 - Assignments</h1>
+
+    <div>
+      <p>Name: Jean-Pierre Nde-Forgwang</p>
+      <p>Course Section: CPRG 306 A</p>
+      <p>
+        <a href="https://github.com/">GitHub</a>
+      </p>
+    </div>
+    
     </main>
+
   );
 }
+
