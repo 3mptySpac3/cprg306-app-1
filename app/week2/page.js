5596f6b0850bab1fd664386016aee30084af0c9c
3mptySpac3 <thefantasticalproject@gmail.com>
2023-09-23 00:30:52 -0600

    Final week 2
    Everything is done, week 2 assignment complete, link to week 2 page, flex added to week 2 page

--- app/week2/page.js
+++ app/week2/page.js
@@ -7,11 +7,13 @@ export default function Week2() {
   return (
     <div>
       <Navbar />
-      <div className="mt-24">
-        <h1 className='text-4xl ml-36 m-2'>My Shopping List</h1>
-      </div>
-      <div>
-        <UserInfo />
+      <div className="flex justify-between items-center">
+        <div className="mt-24 ml-36">
+          <h1 className='text-4xl m-2 '>My Shopping List</h1>
+        </div>
+        <div className=" mr-12 mt-24">
+          <UserInfo />
+        </div>
       </div>
     </div>
   )
