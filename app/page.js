07e149d2dd0fd9f62a45382714e13b671749fbe4
3mptySpac3 <thefantasticalproject@gmail.com>
2023-09-22 22:46:21 -0600

    Fixing Main page

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Page() {
       <div className = "mt-24">
         <h1 className = 'text-4xl m-2 ml-36'>CPRG 306: Web Development 2 - Assignments</h1>
 
-        <div>
+        <div className = 'ml-36 '>
           <p>Name: Jean-Pierre Nde-Forgwang</p>
           <p>Course Section: CPRG 306 A</p>
           <p>
