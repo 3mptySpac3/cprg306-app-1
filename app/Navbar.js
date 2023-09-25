b8158b53d7c0ea16b0a832adabe46b3ed0c14557
3mptySpac3 <thefantasticalproject@gmail.com>
2023-09-25 12:41:21 -0600

    Navbar edit
    moved the items from navbar into a list, this way each has its own space, and now I can manipulate each without affecting the rest, also did a "flex" style so everything appears horizontal instead of vertical

--- app/Navbar.js
+++ app/Navbar.js
@@ -7,14 +7,19 @@ export default function Navbar() {
   return (
     <nav className="fixed top-0 left-0 w-full bg-black text-white h-16 mt-1 mb-1 p-10 border-2 border-white rounded hover:shadow-xl transition-shadow duration-300">
       <div className="container mx-auto flex justify-center items-center h-full">
-        <div style= {{fontFamily: "cursive"}} className="space-x-10">
-          <CustomLink href="/">Home</CustomLink>
-          <CustomLink target="_blank" href="/week2">Week-II</CustomLink>
-          <CustomLink href="/week3">Week-III</CustomLink>
-          <CustomLink href="/week4">Week-IV</CustomLink>
-          <CustomLink href="/week5">Week-V</CustomLink>
-          <CustomLink href="/week6">Week-VI</CustomLink>
-          <CustomLink href="/week7">Week-VII</CustomLink>
+        <div style= {{fontFamily: "cursive"}}>
+          <ul className= "flex space-x-2">
+            <li><CustomLink href="/">Home</CustomLink></li>
+            <li><CustomLink href="/week2">Week-II</CustomLink></li>
+            <li><CustomLink href="/week3">Week-III</CustomLink></li>
+            <li><CustomLink href="/week4">Week-IV</CustomLink></li>
+            <li><CustomLink href="/week5">Week-V</CustomLink></li>
+            <li><CustomLink href="/week6">Week-VI</CustomLink></li>
+            <li><CustomLink href="/week7">Week-VII</CustomLink></li>
+            <li><CustomLink href="/week8">Week-VIII</CustomLink></li>
+            <li><CustomLink href="/week9">Week-IX</CustomLink></li>
+            <li><CustomLink href="/week10">Week-X</CustomLink></li>
+          </ul>
         </div>
       </div>
     </nav>
