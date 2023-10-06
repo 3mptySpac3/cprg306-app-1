09794a5d9592df4b2ec614b3e524e77348a98be9
3mptySpac3 <thefantasticalproject@gmail.com>
2023-10-06 12:43:35 -0600

    i think i got it

--- app/week4.11/week4/page.js
+++ app/week4.11/week4/page.js
@@ -1,7 +1,7 @@
 
-import React from 'react'
-import Navbar from '../../Navbar'
-import FoodForm from './FoodForm'
+import React from 'react';
+import Navbar from '../../Navbar';
+import FoodForm from './FoodForm';
 
 export default function Week4() {
   return (
