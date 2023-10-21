07fe4855b907e4430788e477a41c7665c068d47b
3mptySpac3 <thefantasticalproject@gmail.com>
2023-10-21 16:12:45 -0600

    week 2, text color, white

--- app/week2/page.js
+++ app/week2/page.js
@@ -6,7 +6,7 @@ export default function Week2() {
   return (
     <div>
       <Navbar />
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center text-white">
         <div className="mt-4 ml-36">
           <h1 className='text-4xl m-2 '>My Shopping List</h1>
         </div>
